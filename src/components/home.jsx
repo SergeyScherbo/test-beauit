f765a103c6e8cd56653c7e05b4d3686595a3f960
Scherbo <sergey31scherbo@gmail.com>
2018-10-25 19:37:12 +0400

    very basic home component

---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<React.Fragment>
+			<h1>Home</h1>
+			<p>Hey, this is nice and clean SPA based on ReactJS</p>
+		</React.Fragment>
+	);
+}
+
+export default Home;
