d9e594314f6e2285360223f537b9baca268fea6c
Scherbo <sergey31scherbo@gmail.com>
2018-10-25 23:28:35 +0400

    small refactor

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { getUserFromDB } from '../utils/users';
 
 class Login extends Component {
 	state = {
@@ -9,15 +10,16 @@ class Login extends Component {
 
 	handleSubmit = (event) => {
 		event.preventDefault();
+		const { username, password } = this.state;
 
-		if (this.state.username !== 'Admin' || this.state.password !== '12345') {
+		if (getUserFromDB({ username, password }) === undefined) {
 			this.setState({
 				username: '',
 				password: '',
 				error: 'Incorrect user or password...'
 			})
 		} else {
-			this.props.onLogin({ username: this.state.username, password: this.state.password });
+			this.props.onLogin({ username, password });
 			this.props.history.push('/');
 		}
 	}
@@ -30,17 +32,19 @@ class Login extends Component {
 	}
 
 	render() {
+		const { username, password } = this.state;
+
 		return (
 			<React.Fragment>
 				<h1>Sign in</h1>
 				<form onSubmit={this.handleSubmit}>
 					<div className="field-wrp">
 						<label className="label" htmlFor=""></label>
-						<input onChange={this.handleChange} value={this.state.username} name="username" className="field" type="text" placeholder="Login" />
+						<input onChange={this.handleChange} value={username} name="username" className="field" type="text" placeholder="Login" />
 					</div>
 					<div className="field-wrp">
 						<label className="label" htmlFor=""></label>
-						<input onChange={this.handleChange} value={this.state.password} name="password" className="field" type="password" placeholder="Password" />
+						<input onChange={this.handleChange} value={password} name="password" className="field" type="password" placeholder="Password" />
 					</div>
 					<div className="button-wrp">
 						<button disabled={this.basicValidate()} className="button button--primary">Submit</button>
@@ -52,7 +56,8 @@ class Login extends Component {
 	}
 
 	basicValidate() {
-		return this.state.username.length === 0 || this.state.password.length === 0;
+		const { username, password } = this.state;
+		return username.length === 0 || password.length === 0;
 	}
 }
 
